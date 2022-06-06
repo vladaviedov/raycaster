215b5b1e6ac470abe280b7168b669afcab3f46b3
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-06 09:22:28 -0400

    gray window

--- src/main.c
+++ src/main.c
@@ -1,3 +1,33 @@
-int main() {
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+#define WIDTH 512
+#define LENGTH 512
+#define WIN_NAME "rc"
+
+void init(void);
+void loop(void);
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WIDTH, LENGTH);
+	glutCreateWindow(WIN_NAME);
+	init();
+
+	glutDisplayFunc(&loop);
+	glutMainLoop();
+
 	return 0;
+}
+
+void init(void) {
+	glClearColor(0.3, 0.3, 0.3, 0);
+	gluOrtho2D(0, WIDTH, LENGTH, 0);
+}
+
+void loop(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+	glutSwapBuffers();
 }
