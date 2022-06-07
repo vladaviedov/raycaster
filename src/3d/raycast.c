85ff2d1e4b3ebd6f8717e3091ea9af004330f104
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-07 10:58:27 -0400

    raycast file

---
+++ src/3d/raycast.c
@@ -0,0 +1,5 @@
+#include "raycast.h"
+
+int cast_ray(double x, double y, double th) {
+	return 0;
+}
---
+++ src/3d/raycast.h
@@ -0,0 +1,6 @@
+#ifndef RC_3D_RAYCAST_H
+#define RC_3D_RAYCAST_H
+
+int cast_ray(double x, double y, double th);
+
+#endif // RC_3D_RAYCAST_H
