d3f52034c6a522d9340ce5b54363c89d950806f3
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-07 19:24:17 -0400

    1 block hor cast

--- src/map/map.h
+++ src/map/map.h
@@ -3,13 +3,15 @@
 
 typedef enum {
 	VOID,
-	WALL
+	WALL,
+	OOB
 } mp_cell_val;
 
 int map_new(int xdim, int ydim);
 int map_set_spawn(int x, int y);
 int map_del(void);
 int map_set_cell(int x, int y, mp_cell_val val);
+mp_cell_val map_get_cell(int x, int y);
 void map_draw(void);
 
 #endif // RC_MAP_MAP_H
