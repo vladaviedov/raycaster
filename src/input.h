47b24a8ad359fcace3dd69a49edb1e0faf87b98b
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-08 12:18:11 -0400

    mouse movement

--- src/input.h
+++ src/input.h
@@ -7,8 +7,10 @@ extern int key_d;
 extern int key_a;
 extern int key_k;
 extern int key_l;
+extern double mouse_x;
 
 void i_keydown(unsigned char key, int x, int y);
 void i_keyup(unsigned char key, int x, int y);
+void i_mouse(int x, int y);
 
 #endif // RC_INPUT_H
